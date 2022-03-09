e9c2589d8b70e97a08482048249d04d83758e63f
Hector J Estrada Toledo <hjestrada@misena.edu.co>
2022-03-09 11:41:31 -0500

    dfghjk

--- Inventario Laboratorio/Estantes.vb
+++ Inventario Laboratorio/Estantes.vb
@@ -7,9 +7,20 @@ Imports System.Data.SQLite
 Imports System.Data.SqlClient
 Imports System.IO
 
-
-
 Public Class Estantes
+
+
+
+    Dim DB_Path As String = "Data Source=" & Application.StartupPath & "\BD_Lab.s3db;"
+    Dim SQLiteCon As New SQLiteConnection(DB_Path)
+    Dim SQLliteCMD As New SQLiteCommand
+    Dim Imag As Byte()
+    Dim consulta1 As String
+    Dim lista1 As Byte
+    Dim datos1 As DataSet
+    Dim MySQLDA1 As New SQLiteDataAdapter
+
+
 
 
     'Necesarios para redondear formulario
@@ -57,7 +68,103 @@ Public Class Estantes
     Private Sub Estantes_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'Necesario para redondear formulario
         Me.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Width - 2, Height - 2, 20, 20))
+        MAXID()
+        CargarDatos()
+
+    End Sub
+
+    Private Sub IconButton1_Click(sender As Object, e As EventArgs) Handles IconButton1.Click
+
+        If Trim(TextBox2.Text) = "" Then
+            MsgBox("¡Error! no se permiten campos vacios")
+
+        Else
+            SQLiteCon.Close()
+
+            Try
+                SQLiteCon.Open()
+                SQLliteCMD = New SQLiteCommand
+
+                With SQLliteCMD
+                    .CommandText = " INSERT INTO ESTANTE (`ID_ESTANTE`, `DESCRIPCION`) VALUES (NULL, @DESCRIPCION)"
+                    .Connection = SQLiteCon
+                    .Parameters.AddWithValue("@DESCRIPCION", Me.TextBox2.Text)
+                    .ExecuteNonQuery()
+                End With
+
+                SQLiteCon.Close()
+                MsgBox("Datos Registrados Exitosamente")
+                Me.TextBox2.Clear()
+                CargarDatos()
+                MAXID()
 
 
+            Catch ex As Exception
+                MsgBox("Error" & vbCr & ex.Message, MsgBoxStyle.Critical, "Error Message")
+                SQLiteCon.Close()
+                Return
+            End Try
+            SQLiteCon.Close()
+
+
+        End If
+
+
+    End Sub
+
+
+    Private Sub CargarDatos()
+
+        Dim sql As String = "SELECT `ID_ESTANTE`,`DESCRIPCION` FROM ESTANTE"
+
+        Using con As New SQLiteConnection(DB_Path)
+            Dim command As New SQLiteCommand(sql, con)
+            Dim da As New SQLiteDataAdapter
+            da.SelectCommand = command
+            Dim dt As New DataTable
+            da.Fill(dt)
+            DataGridView1.DataSource = dt
+
+        End Using
+    End Sub
+
+    Sub MAXID()
+
+        MySQLDA1.SelectCommand = New SQLiteCommand
+        MySQLDA1.SelectCommand.Connection = SQLiteCon
+        MySQLDA1.SelectCommand.CommandText = "SELECT MAX(`ID_ESTANTE`)  AS id FROM ESTANTE"
+        SQLiteCon.Open()
+        Dim valorDefecto As Integer = 1
+        Dim ValorRetornado As Object = MySQLDA1.SelectCommand.ExecuteScalar()
+
+        If ValorRetornado Is DBNull.Value Then
+            Label3.Text = CStr(valorDefecto)
+        Else
+            Label3.Text = CStr(ValorRetornado) + 1
+        End If
+        SQLiteCon.Close()
+    End Sub
+
+    Private Sub IconButton4_Click(sender As Object, e As EventArgs) Handles IconButton4.Click
+
+        Try
+
+
+            SQLiteCon.Open()
+            Dim Numero As String
+            Numero = InputBox("Por favor digite el identificador del Estante:")
+
+            If MessageBox.Show("¿Seguro que desea eliminar este registro?", "Atencion", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = vbYes Then
+                SQLliteCMD = New SQLite.SQLiteCommand("delete from ESTANTE where ID_ESTANTE='" & Numero & "'", SQLiteCon)
+                SQLliteCMD.ExecuteNonQuery()
+                CargarDatos()
+                SQLiteCon.Close()
+                MAXID()
+
+            End If
+
+        Catch ex As Exception
+            MsgBox("Operación eliminar cancelada por el usuario")
+        End Try
     End Sub
 End Class
